db15a775dfb6f54f1b9fe7a1570e55ccb8e03737
Aniket Kumar Gupta <aniketguptag6@gmail.com>
2026-06-27 16:28:02 +0530

    0443_String Compression.java

---
+++ 0001-1000/0443_String Compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        int read=0;
+        int write=0;
+
+        while(read < chars.length){
+            char current = chars[read];
+            int count = 0;
+
+            while(read < chars.length && chars[read]==current){
+                read++;
+                count++;
+            }
+            chars[write++]=current;
+            if (count > 1) {
+                String cnt = String.valueOf(count);
+                for (char c : cnt.toCharArray()) {
+                    chars[write++] = c;
+                }
+            }    
+        }
+        return write;
+    }
+}
