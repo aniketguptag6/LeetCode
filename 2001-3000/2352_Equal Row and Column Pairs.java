861aa395e19e2ebd5b6a39a7b27ce148dd695351
Aniket Kumar Gupta <aniketguptag6@gmail.com>
2026-07-08 11:12:45 +0530

    2352_Equal Row and Column Pairs.java

---
+++ 2001-3000/2352_Equal Row and Column Pairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+         int n = grid.length;
+        Map<String, Integer> map = new HashMap<>();
+
+        // Store each row as a string
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb.append(grid[i][j]).append(",");
+            }
+            String row = sb.toString();
+            map.put(row, map.getOrDefault(row, 0) + 1);
+        }
+        int count = 0;
+
+        // Compare each column with stored rows
+        for (int j = 0; j < n; j++) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                sb.append(grid[i][j]).append(",");
+            }
+            String col = sb.toString();
+            count += map.getOrDefault(col, 0);
+        }
+
+        return count;
+    }
+}
