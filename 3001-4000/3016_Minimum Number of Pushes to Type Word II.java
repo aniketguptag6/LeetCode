7002db30e947d26e2aa5199a2c02343905921a91
Aniket Kumar Gupta <aniketguptag6@gmail.com>
2026-07-31 10:35:55 +0530

    3016_Minimum Number of Pushes to Type Word II.java

---
+++ 3001-4000/3016_Minimum Number of Pushes to Type Word II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int pushes = 0;
+        int position = 0;
+
+        // Traverse from largest frequency to smallest
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+
+            int cost = position / 8 + 1;
+            pushes += freq[i] * cost;
+            position++;
+        }
+
+        return pushes;
+    }
+}
