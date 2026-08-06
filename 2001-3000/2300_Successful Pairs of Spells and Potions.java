250cb8023f66a649f03a7abf6c6f80e812978ab9
Aniket Kumar Gupta <aniketguptag6@gmail.com>
2026-08-06 07:16:39 +0530

    2300_Successful Pairs of Spells and Potions.java

---
+++ 2001-3000/2300_Successful Pairs of Spells and Potions.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+
+        int m = spells.length;
+        int n = potions.length;
+        int[] ans = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int left = 0, right = n - 1;
+            int idx = n;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if ((long) spells[i] * potions[mid] >= success) {
+                    idx = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans[i] = n - idx;
+        }
+
+        return ans;
+    }
+}
